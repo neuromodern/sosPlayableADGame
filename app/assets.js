60b68e69ca450397b4425a37f544f3a958a28828
adsidesandbox <93096842+adsidesandbox@users.noreply.github.com>
2021-10-24 22:39:48 +0300

    Add files via upload

---
+++ app/assets.js
@@ -0,0 +1,118 @@
+var bgSpriteTextureFile="assets/bg.jpg";
+
+
+var enemyTexture=[];
+enemyTexture[0] = "assets/enemy/0.png";
+enemyTexture[1] = "assets/enemy/1.png";
+enemyTexture[2] = "assets/enemy/2.png";
+enemyTexture[3] = "assets/enemy/3.png";
+enemyTexture[4] = "assets/enemy/4.png";
+enemyTexture[5] = "assets/enemy/5.png";
+enemyTexture[6] = "assets/enemy/6.png";
+enemyTexture[7] = "assets/enemy/7.png";
+enemyTexture[8] = "assets/enemy/8.png";
+enemyTexture[9] = "assets/enemy/9.png";
+enemyTexture[10] = "assets/enemy/10.png";
+enemyTexture[11] = "assets/enemy/11.png";
+//attack
+enemyTexture[12] = "assets/enemy/attack/00.png";
+enemyTexture[13] = "assets/enemy/attack/01.png";
+enemyTexture[14] = "assets/enemy/attack/02.png";
+enemyTexture[15] = "assets/enemy/attack/03.png";
+enemyTexture[16] = "assets/enemy/attack/04.png";
+enemyTexture[17] = "assets/enemy/attack/05.png";
+enemyTexture[18] = "assets/enemy/attack/06.png";
+enemyTexture[19] = "assets/enemy/attack/07.png";
+enemyTexture[20] = "assets/enemy/attack/08.png";
+enemyTexture[21] = "assets/enemy/attack/09.png";
+enemyTexture[22] = "assets/enemy/attack/10.png";
+enemyTexture[23] = "assets/enemy/attack/11.png";
+enemyTexture[24] = "assets/enemy/attack/12.png";
+//death
+enemyTexture[25] = "assets/enemy/death/00.png";
+enemyTexture[26] = "assets/enemy/death/01.png";
+enemyTexture[27] = "assets/enemy/death/02.png";
+enemyTexture[28] = "assets/enemy/death/03.png";
+enemyTexture[29] = "assets/enemy/death/04.png";
+enemyTexture[30] = "assets/enemy/death/05.png";
+enemyTexture[31] = "assets/enemy/death/06.png";
+enemyTexture[32] = "assets/enemy/death/07.png";
+enemyTexture[33] = "assets/enemy/death/08.png";
+enemyTexture[34] = "assets/enemy/death/09.png";
+enemyTexture[35] = "assets/enemy/death/10.png";
+enemyTexture[36] = "assets/enemy/death/11.png";
+enemyTexture[37] = "assets/enemy/death/12.png";
+
+
+
+
+
+
+
+  //enemyTexture[12] = "assets/enemy/death/death_00.png";
+//  enemyTexture[13] = "assets/enemy/death/death_01.png";
+ // enemyTexture[14] = "assets/enemy/death/death_02.png";
+   //enemyTexture[15] = "assets/enemy/death/death_03.png";
+//enemyTexture[16] = "assets/enemy/death/death_04.png";
+  //   	enemyTexture[17] = "assets/enemy/death/death_05.png";
+// 		enemyTexture[18] = "assets/enemy/death/death_06.png";
+ //			enemyTexture[19] = "assets/enemy/death/death_07.png";
+   //  				enemyTexture[20] = "assets/enemy/death/death_08.png";
+//					enemyTexture[21] = "assets/enemy/death/death_09.png";
+  //	enemyTexture[22] = "assets/enemy/death/death_10.png";
+// 							enemyTexture[23] = "assets/enemy/death/death_11.png";
+
+
+
+
+var weapon1TextureFiles=[];
+for(let i=0; i<16;i++){
+weapon1TextureFiles[i] = "assets/weapon1/" + i + ".png";
+
+}
+
+var weapon1FireTextureFiles=[];
+for(let i=0; i<16;i++){
+weapon1FireTextureFiles[i] = "assets/fire1/" + i + ".png";
+
+}
+
+
+var weapon2TextureFiles=[];
+for(let i=0; i<16;i++){
+weapon2TextureFiles[i] = "assets/weapon2/" + i + ".png";
+
+}
+
+var weapon2FireTextureFiles=[];
+for(let i=0; i<16;i++){
+weapon2FireTextureFiles[i] = "assets/fire2/" + i + ".png";
+
+}
+
+
+var weapon3TextureFiles=[];
+for(let i=0; i<16;i++){
+weapon3TextureFiles[i] = "assets/weapon3/" + i + ".png";
+
+}
+
+var weapon3FireTextureFiles=[];
+for(let i=0; i<16;i++){
+weapon3FireTextureFiles[i] = "assets/fire3/" + i + ".png";
+
+}
+
+var weaponFiringTextureFiles=[];
+weaponFiringTextureFiles[0]="assets/firing/0.png";
+weaponFiringTextureFiles[1]="assets/firing/1.png";
+weaponFiringTextureFiles[2]="assets/firing/2.png";
+weaponFiringTextureFiles[3]="assets/firing/3.png";
+
+
+
+
+var uiPanelTextureFile="assets/ui_panel.png";
+var logoTextureFile="assets/logo-min.png";
+var uiBoxTextureFile="assets/ui_box.png";
+var uiBalanceTextureFile="assets/ui_balance.png";
